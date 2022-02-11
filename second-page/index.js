74cc8bcd870b6159a8daedbb8e24036128453e08
Spike Vinz <spikevinz@gmail.com>
2022-02-12 07:22:46 +0800

    finish thank you page

--- second-page/index.js
+++ second-page/index.js
@@ -1,13 +1,15 @@
-console.log("hello");
 (function () {
   var queryString = window.location.search;
   var parameters = new URLSearchParams(queryString);
+
   var email = parameters.get("email");
   var price = parameters.get("price");
 
   var domEmail = document.querySelector("#email");
   var domPrice = document.querySelector("#price");
 
-  domEmail.textContent = `Thank you ${email}!`;
-  domPrice.textContent = `You purchased the [${price}] option!`;
+  if (email && price) {
+    domPrice.innerHTML = `Congratulations! You now have <strong>${price}</strong> access to Chrono Block.`;
+    domEmail.innerHTML = `Check your email: <strong>${email}</strong> to start using the app now. Enjoy!`;
+  }
 })();
